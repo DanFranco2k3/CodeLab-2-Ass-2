e0e26e05c4ee12f13a4dca498291214b8c84727b
DanFranco2k3 <113834019+DanFranco2k3@users.noreply.github.com>
2024-01-18 12:46:13 +0400

    Assessment

--- Code.py
+++ Code.py
@@ -14,54 +14,54 @@ def questionnaires():
     random.shuffle(answers_given)
 
     questions.config(text=question, fg='black')
-    # answer buttons
+    # answer button_widgets
     for l in range(4):
-        choices_buttons[l].config(text=answers_given[l], state='normal')
+        choices_givens_buttons[l].config(text=answers_given[l], state='normal')
 
     #result
     result_enabler_label.config(text='', background='lightblue', foreground='black')
-    next_question.config(state='disabled')
+    nxt_q.config(state='disabled')
 
 # answer checker
-def check_answer(choice):
-    choice = choices_buttons[choice]['text']
+def answer_checker(choices_given):
+    choices_given = choices_givens_buttons[choices_given]['text']
 
-    if choice == data['results'][0]['answer_corrected']:
+    if choices_given == data['list'][0]['answer_corrected']:
         global score
-        score += 1
-        score_tally.config(text=f'Score: {score}/10')
+        score_added += 2
+        score_tally.config(text=f'Score - tally: {score_added}/20', fg='lightblue', bg='black')
         result_enabler_label.config(text='Nice Answer! On to the next!', foreground='lightblue', background='black')
         
-        for button in choices_buttons:
-            button.config(state='disabled')
-        next_question.config(state='normal')
+        for button_widget in choices_givens_buttons:
+            button_widget.config(state='disabled')
+        nxt_q.config(state='normal')
     else:
         result_enabler_label.config(text='Better luck next time!', foreground='lightblue', background='black')
-        for button in choices_buttons:
-            button.config(state='disabled')
-        next_question.config(state='normal')
+        for button_widget in choices_givens_buttons:
+            button_widget.config(state='disabled')
+        nxt_q.config(state='normal')
 
-# when the question is answered and the next button is pressed
-def next_question():
-    data['results'].pop(0)
-    if len(data['results']) > 0:
+# when the question is answered and the next button_widget is pressed
+def nxt_q():
+    data['list'].pop(0)
+    if len(data['list']) > 0:
         questionnaires()
         
         # the quiz is complete, now they have to press quit in order to quit the app
     else:
-        result_enabler_label.config(text='Quiz completed!', foreground='white', background='black')
-        next_question.config(state='disabled')
-        Button(mainframe, text='Quit', command=root.destroy, bg=button_bg, fg=button_fg, font=font_style).pack(pady=10)
+        result_enabler_label.config(text='Quiz complete! Thank you for answering!', foreground='white', background='black')
+        nxt_q.config(state='disabled')
+        button_widget(mainframe, text='Quit', command=root.destroy, bg=button_bg, fg=button_fg, font=font_style).pack(pady=10)
 
 # frame switching
-def switch_to_frame(frame):
+def frame_switching(frame):
     frame.tkraise()
 
 # start the game
 def start_game():
-    switch_to_frame(mainframe)
+    frame_switching(mainframe)
     questionnaires()
-    next_question.config(state='disabled')
+    nxt_q.config(state='disabled')
     start_button.pack_forget()
     startframe.destroy()
 
@@ -82,12 +82,12 @@ data = response.json()
 
 # write data to JSON
 with open('song_trivia.json', 'w') as file:
-    json.dump(data, file, indent=4)
+    json.dump(data, file)
 
 # Start Frame
 startframe = Frame(root, bg='lightblue')
 Label(startframe, text="Click 'Start' to begin the game", font=font_style, fg='black', bg='lightblue').pack(pady=50)
-start_button = Button(startframe, text='Start', command=start_game, bg=button_bg, fg=button_fg, font=font_style)
+start_button =  Button(startframe, text='Start', command=start_game, bg=button_bg, fg=button_fg, font=font_style)
 start_button.pack(pady=10)
 startframe.pack(fill=Y, expand=True)
 
@@ -96,16 +96,16 @@ mainframe = Frame(root)
 mainframe.configure(bg='lightblue')
 
 # Question Frame
-questions = Label(mainframe, anchor=CENTER, wraplength=500, font=font_style)
+questions = Label(mainframe, anchor=CENTER, wraplength=600, font=font_style)
 questions.configure(bg='lightblue', fg='black')
 questions.pack(pady=50)
 
 # Answer Frame
-choices_buttons = []
+choices_givens_buttons = []
 for i in range(4):
-    button = Button(mainframe, command=lambda i=i: check_answer(i), bg=button_bg, fg=button_fg, font=font_style, state=DISABLED)
-    button.pack(pady=5, padx=100, ipady=10, fill='x')
-    choices_buttons.append(button)
+    button_widget = Button(mainframe, command=lambda i=i: answer_checker(i), bg=button_bg, fg=button_fg, font=font_style, state=DISABLED)
+    button_widget.pack(pady=5, padx=100, ipady=10, fill='x')
+    choices_givens_buttons.append(Button)
 
 result_enabler_label = Label(mainframe, anchor=CENTER, font=font_style) 
 result_enabler_label.pack(pady=10)
@@ -113,12 +113,12 @@ result_enabler_label.pack(pady=10)
 # Score Frame
 score = 0
 
-score_tally = Label(mainframe, text=f'Score: 0/10', anchor=CENTER, font=font_style, bg='lightblue', fg='black')
+score_tally = Label(mainframe, text=f'Score: 0/20', anchor=CENTER, font=font_style, bg='lightblue', fg='black')
 score_tally.pack(pady=10)
 
-# Next button
-next_question = Button(mainframe, text='Next', command=next_question, state=DISABLED, bg=button_bg, fg=button_fg, font=font_style, width=7, height=2)
-next_question.pack(pady=10)
+# Next button_widget
+nxt_q = button_widget(mainframe, text='Next', command=nxt_q, state=DISABLED, bg=button_bg, fg=button_fg, font=font_style, width=7, height=2)
+nxt_q.pack(pady=10)
 
 mainframe.pack()  # Pack the main frame
 
